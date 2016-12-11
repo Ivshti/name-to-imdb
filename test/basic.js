f2e4ad1813602cef765f865e1fa865e3c48600fc
Ivo Georgiev <ivo@linvo.org>
2016-12-11 20:21:00 +0200

    tests: add two separate tests for google/non-google matches

--- test/basic.js
+++ test/basic.js
@@ -1,11 +1,22 @@
 var nameToImdb = require("../index");
 var tape = require("tape")
 tape("basic test", function(t) {
+	nameToImdb({ name: "south park", type: "series" }, function(err, res, inf) {
+		t.error(err);
+		t.equals(res, "tt0121955")
+		t.ok(inf)
+		t.ok(inf.match && inf.match === "metadata", "is matched on cinemeta");
+		t.end()
+	})
+})
+
+tape("basic test - match from google (type not provided)", function(t) {
 	nameToImdb({ name: "south park" }, function(err, res, inf) {
 		t.error(err);
-        t.equals(res, "tt0121955")
-        console.log(inf); // inf contains info on where we matched that name - e.g. locally, or on google
+		t.equals(res, "tt0121955")
+		t.ok(inf)
+		t.ok(inf.match && inf.match.match("google"), "is matched on google");
+		t.end()
+		//console.log(inf); // inf contains info on where we matched that name - e.g. locally, or on google
 	})
-
-
 })
