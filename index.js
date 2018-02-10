67df03490ef11747a3d5f05740aa7d1882c4ca5f
Ivo Georgiev <ivo@linvo.org>
2018-02-10 20:13:09 +0200

    Readability and style

--- index.js
+++ index.js
@@ -1,51 +1,59 @@
-var namedQueue = require("named-queue");
-var _ = require("lodash");
-var helpers = require("./helpers")
+var namedQueue = require('named-queue');
+var _ = require('lodash');
+var helpers = require('./helpers')
 
-var metadataFind = require("./providers/cinemeta")
-var imdbFind = require("./providers/imdbFind")
+var metadataFind = require('./providers/cinemeta')
+var imdbFind = require('./providers/imdbFind')
 
 // Constants
-var CACHE_TTL = 12*60*60*1000; // if we don't find an item, how long does it stay in the cache as "not found" before we retry it 
+var CACHE_TTL = 12*60*60*1000; // if we don't find an item, how long does it stay in the cache as 'not found' before we retry it 
 
 // In-memory cache for matched items, to avoid flooding Google (or whatever search api we use)
 var cache = { };
 
+// Named queue, means that we're not working on one name more than once at a tim
+// and a total of 3 names at once
+var queue = new namedQueue(worker, 3)
+
 // Outside API
 function nameToImdb(args, cb) {
-    args = typeof(args)=="string" ? { name: args } : args;
+    args = typeof(args)=='string' ? { name: args } : args
 
-    args.name = helpers.simplifyName(args);
+    args.name = helpers.simplifyName(args)
     
-    var q = _.pick(args, "name", "year", "type");
+    var q = _.pick(args, 'name', 'year', 'type')
 
-    if (! q.name) return cb(new Error("empty name"));
+    if (! q.name) return cb(new Error('empty name'))
 
-    if (q.year && typeof(q.year)=="string") q.year = parseInt(q.year.split("-")[0]);
-    if (q.year && isNaN(q.year)) return cb(new Error("invalid year"));
+    if (q.year && typeof(q.year)=='string') q.year = parseInt(q.year.split('-')[0])
+    if (q.year && isNaN(q.year)) return cb(new Error('invalid year'))
 
-    if (q.type && !(q.type=="movie" || q.type=="series")) return cb(null, null); // no match for other types
+    if (q.type && !(q.type=='movie' || q.type=='series')) 
+        return cb(null, null) // no match for other types
 
-    var hash = new Buffer(args.hintUrl || _.values(q).join(":")).toString("ascii"); // convert to ASCII since EventEmitter bugs with UTF8
-    if (cache.hasOwnProperty(hash)) return cb(null, cache[hash]);
+    var key = new Buffer(args.hintUrl || _.values(q).join(':')).toString('ascii') // convert to ASCII since EventEmitter bugs with UTF8
+    
+    if (cache.hasOwnProperty(key))
+        return cb(null, cache[key])
 
-    queue.push({ id: hash, q: q, args: args }, function(err, imdb_id, match) {
-        if (err) return cb(err);
+    queue.push({ id: key, q: q, args: args }, function(err, imdb_id, match) {
+        if (err) 
+            return cb(err)
+        
         if (imdb_id) {
-            cache[hash] = imdb_id;
-            setTimeout(function() { delete cache[hash] }, CACHE_TTL);
+            cache[key] = imdb_id
+            setTimeout(function() { delete cache[key] }, CACHE_TTL)
         }
-        cb(null, imdb_id, match);
-    });
-};
 
-var queue = new namedQueue(worker, 3);
+        cb(null, imdb_id, match)
+    })
+};
 
 function worker(task, cb) {
     // Find it in our metadata, if not, fallback to IMDB API, then Google
     metadataFind(task.q, function(err, id) {
         if (err) return cb(err);
-        if (id) return cb(null, id, { match: "metadata" });
+        if (id) return cb(null, id, { match: 'metadata' })
         imdbFind(task.q, cb);
     })
 }
