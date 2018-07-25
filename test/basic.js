4b631dcb6791a7294d67fd6e287ee21f29a436a7
Ivo Georgiev <ivo@strem.io>
2018-07-26 00:17:09 +0300

    test fixed

--- test/basic.js
+++ test/basic.js
@@ -1,12 +1,12 @@
 var nameToImdb = require("../index");
 var tape = require("tape")
 
-tape("basic test - match from google (type not provided)", function(t) {
+tape("basic test - match from imdb (type not provided)", function(t) {
 	nameToImdb({ name: "south park" }, function(err, res, inf) {
 		t.error(err);
 		t.equals(res, "tt0121955")
-		t.ok(inf)
-		t.ok(inf.match && inf.match.match("google"), "is matched on google");
+		t.ok(inf, 'has inf')
+		t.ok(inf.match && inf.match.match("imdbFind"), "is matched on imdb");
 		t.end()
 		//console.log(inf); // inf contains info on where we matched that name - e.g. locally, or on google
 	})
