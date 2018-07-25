4103426a80b6908d07590853d4ae7d6ee945d880
Ivo Georgiev <ivo@strem.io>
2018-07-26 00:25:33 +0300

    remove stremio-addons dep, moved to using cinemeta URLs directly

--- providers/cinemeta.js
+++ providers/cinemeta.js
@@ -1,11 +1,11 @@
-var Stremio = require("stremio-addons");
-
+var fetch = require("node-fetch")
 var helpers = require("../helpers")
 
 // Use cinemeta to pull names of movies and series against IMDB IDs
-var stremio = new Stremio.Client();
-stremio.add("http://cinemeta.strem.io/stremioget/stremio/v1");
-//stremio.add("http://localhost:3005/stremioget/stremio/v1");
+var cinemetaUrls = {
+    'movie': 'https://cinemeta.strem.io/stremioget/stremio/v1/q.json?b=eyJwYXJhbXMiOltudWxsLHt9XSwibWV0aG9kIjoibmFtZXMubW92aWUiLCJpZCI6MSwianNvbnJwYyI6IjIuMCJ9',
+    'series': 'https://cinemeta.strem.io/stremioget/stremio/v1/q.json?b=eyJwYXJhbXMiOltudWxsLHt9XSwibWV0aG9kIjoibmFtZXMuc2VyaWVzIiwiaWQiOjEsImpzb25ycGMiOiIyLjAifQ==',
+}
 
 // Index entry in our in-mem index
 function indexEntry(entry) {
@@ -22,15 +22,21 @@ var meta = { }, byImdb = { };
 
 function metadataFind(query, cb) {
     // It's OK if we don't pass type and we don't fetch names, because we will go to google fallback
-    if (query.type && !pulled[query.type]) stremio.call("names."+query.type, { }, function(err, res) {
-        if (err) console.error(err);
-        if (res) {
+    if (query.type && !pulled[query.type] && cinemetaUrls[query.type]) {
+        fetch(cinemetaUrls[query.type])
+        .then(function(resp) { return resp.json() })
+        .then(function(resp) { return resp.result })
+        .then(function(res) {
             res.forEach(indexEntry);
             pulled[query.type] = 1;
-        }
-        match();
-    });
-    else process.nextTick(match);
+        })
+        .catch(function(e) {
+            console.error(e)
+        })
+        .then(function() {
+            match()
+        })
+    } else process.nextTick(match);
 
     function match() {
         var matches = meta[query.name] || [ ];
