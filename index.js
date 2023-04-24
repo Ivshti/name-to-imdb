274a9aa1bf25ef2c3217cf365b0f95c562b77bf0
dexter21767-dev <dexter21767@gmail.com>
2023-04-24 12:58:17 +0100

    nameToImdb return a promise if no callback is defined

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ var providers = {
 var defaultProviders = ['metadata', 'imdbFind']
 
 // Constants
-var CACHE_TTL = 12*60*60*1000; // if we don't find an item, how long does it stay in the cache as 'not found' before we retry it 
+var CACHE_TTL = 12 * 60 * 60 * 1000; // if we don't find an item, how long does it stay in the cache as 'not found' before we retry it 
 
 // In-memory cache for matched items, to avoid flooding Google (or whatever search api we use)
-var cache = { };
-var cacheLastSet = { };
+var cache = {};
+var cacheLastSet = {};
 
 // Named queue, means that we're not working on one name more than once at a time
 // and a total of 3 names at once
@@ -21,7 +21,15 @@ var queue = new namedQueue(worker, 3)
 
 // Outside API
 function nameToImdb(args, cb) {
-    args = typeof(args)=='string' ? { name: args } : args
+
+    if (!cb)
+        return new Promise((resolve, reject) => nameToImdb(args, function (error, result) {
+            if (error)
+                return reject(error)
+            resolve(result)
+        }))
+
+    args = typeof (args) == 'string' ? { name: args } : args
 
     var q = { name: helpers.parseSearchTerm(args.name) }
     if (args.year) q.year = args.year
@@ -30,27 +38,27 @@ function nameToImdb(args, cb) {
     if (!q.name)
         return cb(new Error('empty name'))
 
-    if (q.year && typeof(q.year)=='string') q.year = parseInt(q.year.split('-')[0])
-    
+    if (q.year && typeof (q.year) == 'string') q.year = parseInt(q.year.split('-')[0])
+
     if (q.year && isNaN(q.year))
         return cb(new Error('invalid year'))
 
-    if (q.type && !(q.type=='movie' || q.type=='series')) 
+    if (q.type && !(q.type == 'movie' || q.type == 'series'))
         return cb(null, null) // no match for other types
     var key = new Buffer.from(args.hintUrl || Object.values(q).join(':')).toString('ascii') // convert to ASCII since EventEmitter bugs with UTF8
-    
-    if (cache.hasOwnProperty(key) && Date.now()-cacheLastSet[key] < CACHE_TTL) {
+
+    if (cache.hasOwnProperty(key) && Date.now() - cacheLastSet[key] < CACHE_TTL) {
         return cb(null, cache[key][0], { match: cache[key][1].match, isCached: true })
     }
 
-    queue.push({ 
+    queue.push({
         id: key,
         q: q,
         providers: args.providers || defaultProviders,
-    }, function(err, res, match) {
+    }, function (err, res, match) {
         if (err)
             return cb(err)
-        
+
         if (res && res.id) {
             cache[key] = [res.id, match]
             cacheLastSet[key] = Date.now()
@@ -65,17 +73,16 @@ function worker(task, cb) {
 
     nextProv()
 
-    function nextProv()
-    {
+    function nextProv() {
         var n = prov.shift()
-        if (! n)
+        if (!n)
             return cb(null, null)
 
         var provider = providers[n]
         if (!provider)
-            return cb(new Error('unknown provider: '+n))
+            return cb(new Error('unknown provider: ' + n))
 
-        provider(task.q, function(err, res) {
+        provider(task.q, function (err, res) {
             if (err)
                 return cb(err)
 
